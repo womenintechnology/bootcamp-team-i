48cdd02870841b4c70eed7e335081e8b4a28dc16
Weronika Jurkiewicz <waj2110@columbia.edu>
2020-08-16 16:46:32 +0200

    removed unnecessary variable from php

--- src/Blog/Controller/Posts.php
+++ src/Blog/Controller/Posts.php
@@ -50,7 +50,7 @@ class Posts implements Common\Controller
 
     private function getPosts(): array
     {
-        $posts = [
+        return [
             ["id" => "1", 
             "title" => "Jak efektywnie nauczyć się programować?", 
             "preamble" => "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).", 
@@ -68,7 +68,5 @@ class Posts implements Common\Controller
             "date" => "20-02-2020",
             "image" => "https://image.freepik.com/foto-gratis/suministros-regreso-escuela_90258-1191.jpg"]
         ];
-
-        return $posts;
     }
 }
