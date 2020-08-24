b8e49369821fabacd035b13a548eddcbd2e1a91b
Justyna <justyna.sowinska@protonmail.com>
2020-08-24 15:28:51 +0200

    graphics from picsum coming soon

--- src/Blog/Controller/Posts.php
+++ src/Blog/Controller/Posts.php
@@ -50,23 +50,28 @@ class Posts implements Common\Controller
 
     private function getPosts(): array
     {
-        return [
-            ["id" => "1", 
-            "title" => "Jak efektywnie nauczyć się programować?", 
-            "preamble" => "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).", 
-            "date" => "10-02-2020",
-            "image" => "/img/BlogPostHeader.jpg"], 
-            
-            ["id" => "2", "title" => "PHP - yay or nay?", 
-            "preamble" => "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).", 
-            "date" => "16-02-2020",
-            "image" => "https://my-test-11.slatic.net/shop/1240135492a9f2e557b7e2e7caebd097.png_1200x1200q75.jpg_.webp"], 
-            
-            ["id" =>"3", 
-            "title" => "PHP, SQL i inne bajery", 
-            "preamble" => "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).", 
-            "date" => "20-02-2020",
-            "image" => "https://image.freepik.com/foto-gratis/suministros-regreso-escuela_90258-1191.jpg"]
-        ];
+        $tablica=[];
+        for ($i=0; $i<3; $i = $i + 1) {
+        array_push($tablica, ["id" => "$i",
+        "title" => "przykład " . (string)($i + 230) . " :) ",
+        "preamble" => "It is a long established fact that a reader will be
+        distracted by the readable content of a page when looking at its
+        layout. The point of using Lorem Ipsum is that it has a more-or-less
+        normal distribution of letters, as opposed to using 'Content here,
+        content here', making it look like readable English. Many desktop
+        publishing packages and web page editors now use Lorem Ipsum as their
+        default model text, and a search for 'lorem ipsum' will uncover many
+        web sites still in their infancy. Various versions have evolved over
+        the years, sometimes by accident, sometimes on purpose (injected
+        humour and the like).",
+        "date" => "10-02-2020",
+        "image" => "https://picsum.photos/id/237/200/300"]
+        );
+        }
+        
+        return $tablica;
+        }
+        
     }
-}
+
+
